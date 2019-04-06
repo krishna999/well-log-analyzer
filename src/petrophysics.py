9aea7065073a08182f5e661aa7b9dbfb6b3472a1
sebasarango1180 <sebasarango1180@gmail.com>
2019-04-05 19:07:18 -0500

    Rethinking of parameter input

--- src/petrophysics.py
+++ src/petrophysics.py
@@ -1,21 +1,42 @@
 """
 Script to make petrophysical analysis over a well log dataset
 
-input: CSV file
+input: path to CSV file (data), path to JSON file (formations)
 output: Petrophysical measurements
+
+JSON example:
+[
+    {
+        "name": "formation_1",
+        "high_depth": 0,
+        "low_depth": 866.5,
+        "fluid_density": 12.4,
+        ...
+    },
+
+    {
+        "name": "formation_2",
+        "high_depth": 866.5,
+        "low_depth": 1366.5,
+        "fluid_density": 10.3,
+        ...
+    }
+]
 
 Author: Sebastian Arango
 
 """
 
 import pandas as pd
-import numpy as np 
+import numpy as np
+import json
 
 
 
 PATH_TO_CSV = '../data/interim/extracted-1.csv'
 MANDATORY_COLUMNS = ['#Depth (#M)', 'GR (API)', 'RT (ohm.m)', 'NPHI (v/v)', 'RHOB (g/cm3)']
 REGION_LIMITS = [0, 866.5, 1366.5, 2217, 2785, 3655, 4770.2]
+FORMATIONS = [] #List of dictionaries, each being a formation.
 
 
 data = pd.read_csv(PATH_TO_CSV)
@@ -24,7 +45,7 @@ def check_data_columns(data):
 
     return False if data[MANDATORY_COLUMNS].isnull().any() else True
 
-def region_asignation(data):
+def region_asignation(FORMATIONS):
 
     """return {
         'A': data.loc[data['#Depth (#M)'] < 866.5],
@@ -36,8 +57,16 @@ def region_asignation(data):
         'G': data.loc[data['#Depth (#M)'] >= 4770.2]"""
         }
 
-    return {str(i): {
-        'data': REGION_LIMITS[i] <= data.loc[data['#Depth (#M)'] < REGION_LIMITS[i+1], 'fd': } for i in len(REGION_LIMITS)-1}
+    """return {str(i): {
+        'data': REGION_LIMITS[i] <= data.loc[data['#Depth (#M)'] < REGION_LIMITS[i+1], 
+        'fd': FLUID_DENSITY[i]},
+        '' for i in len(REGION_LIMITS)-1}"""
+
+    for formation in FORMATIONS:
+
+        formation['data'] = formation.get('high_depth') <= data.loc[data['#Depth (#M)'] < formation.get('low_depth')
+    
+    return FORMATIONS
 
 def 
 
